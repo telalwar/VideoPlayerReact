cf89d80bec41349f64ab433422c59ba5b21bfead
sachin <telalwarsachin@gmail.com>
2017-10-01 22:27:43 -0400

    Modified index.js as a class iI
    :wq

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCJmUEx-dhpAah3fIUoV3_xd5MCdNlHHDU';
-const App = function(){
-  return <div> Hi jkjk</div>
+
+
+
+//creating new Component which produces some html data
+class App extends Component{
+constructor(props){
+  super(props);
+  this.state = { videos: [] };
+
+  YTSearch ({key: API_KEY, term: 'surfboards'}, (videos) =>{
+    this.setState({ videos});
+    console.log();("sdf");
+    });
 }
+
+
+  render() {
+  return  (
+    <div>
+      <SearchBar />
+     </div>
+   );
+ }
+}
+
+// get the Component generated data and put it on pafe in the dom
 ReactDOM.render(<App /> , document.querySelector('.container'));
